13c97004543982d79bb6727d9c3870e8e6c8a9d3
MaferCisneros98 <maria.cisneros.est@tecazuay.edu.ec>
2021-12-01 00:55:00 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,49 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// social login
+import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
+import {
+  GoogleLoginProvider,
+  FacebookLoginProvider
+} from 'angularx-social-login';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { ProductosComponent } from './productos/productos.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MenuComponent,
+    HomeComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocialLoginModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '328276282133-mgu8o0a9ffk1svmubhkq5qspsc5dtcph.apps.googleusercontent.com'
+            )
+          },
+          {
+            id: FacebookLoginProvider.PROVIDER_ID,
+            provider: new FacebookLoginProvider('460117922207508')
+          }
+        ]
+      } as SocialAuthServiceConfig,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {GoogleLoginProvider, SocialAuthService, SocialUser } from 'angularx-social-login';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  socialUser: SocialUser = new SocialUser;
+  userLogged: SocialUser = new SocialUser;
+  isLogged: boolean = false;
+
+  constructor(
+    private authService: SocialAuthService,
+    private router: Router
+    ) { }
+
+  ngOnInit(): void {
+    this.authService.authState.subscribe(
+      data=> {
+        this.userLogged = data;
+        this.isLogged = (this.userLogged != null);
+      }
+    );
+  }
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then(
+      data => {
+        this.socialUser = data;
+        console.log(this.socialUser.idToken);
+        this.isLogged = true;
+        this.router.navigate(['/']);
+      }
+    );
+  }
+
+
+logOut(): void{
+  this.authService.signOut();
+}
+}
